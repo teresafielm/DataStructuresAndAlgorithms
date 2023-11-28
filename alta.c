d30008bb2775c437e5cc6f2cea9994669789ca79
Teresa Fiel Muñoz <37962089+teresafielm@users.noreply.github.com>
2023-11-28 13:48:04 -0600

    Delete alta.c

--- alta.c
+++
@@ -1,9 +0,0 @@
-void alta(char nombre,char telefono,int edad)
-{
-    printf("Ingresa tu nombre: ");
-    scanf("%s",&nombre);
-    printf("Ingresa tu teléfono: ");
-    scanf("%s",&telefono);
-    printf("Ingresa tu edad: ");
-    scanf("%d",&edad);
-}
